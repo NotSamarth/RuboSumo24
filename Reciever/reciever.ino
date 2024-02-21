bdd266c028a3dae09f064ddb717bbb757a92dc72
NotSamarth <samarthvaishnav33@gmail.com>
2024-02-22 00:01:12 +0530

    transmission and receiver modified

--- Reciever/reciever.ino
+++ Reciever/reciever.ino
@@ -1,34 +1,77 @@
+#include <CytronMotorDriver.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+// Define motor pins
+#define LEFT_MOTOR_PWM 3
+#define LEFT_MOTOR_DIR 4
+#define RIGHT_MOTOR_PWM 5
+#define RIGHT_MOTOR_DIR 6
 
-RF24 radio(9,10); // CE, CSN
+// Define joystick pins
+
+
+#define thresholdx 10
+#define thresholdy 10
+#define thresholdstr 10
+// Create motor objects
+CytronMD motorLeft(PWM_DIR,LEFT_MOTOR_PWM, LEFT_MOTOR_DIR);
+CytronMD motorRight(PWM_DIR,RIGHT_MOTOR_PWM, RIGHT_MOTOR_DIR);
+
+RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "12359";
-
-struct JoystickData {
-  int xAxis;
-  int yAxis;
-};
-
-JoystickData joystickData;
+char receivedData[32] = "";
+int  x, y;
 
 void setup() {
   Serial.begin(9600);
+  // Set motor control pins as outputs
+  pinMode(LEFT_MOTOR_PWM, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR, OUTPUT);
+  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR, OUTPUT);
+  Serial.println(-(thresholdy));
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.startListening();
 }
 
 void loop() {
-  joystickData.xAxis = analogRead(A7);
-  joystickData.yAxis = analogRead(A6);
+  // Read joystick values
+  if (radio.available()) {   // If the NRF240L01 module received data
+    radio.read(&receivedData, sizeof(receivedData)); // Read the data and put it into character array
+    x = atoi(&receivedData[0]);
+    Serial.print(x);
+    Serial.print("  "); // Convert the data from the character array (received X value) into integer
+    delay(10);
+    radio.read(&receivedData, sizeof(receivedData));
+    y = atoi(&receivedData[0]);
+    Serial.println(y);
+    delay(10);
+  }
+  
+  
+  
 
-  Serial.print(joystickData.xAxis);
-  Serial.print("  ");
-  Serial.println(joystickData.yAxis);
+ 
+  if( y > thresholdy || (-thresholdy) > y ){    
+    motorLeft.setSpeed(y);
+    motorRight.setSpeed(y);
+    Serial.println("C1");
+  }
+  if( x > thresholdx || (-thresholdx) > x){
+    motorLeft.setSpeed((x));
+    motorRight.setSpeed(x);
+        Serial.println("C2");
 
-  radio.write(&joystickData, sizeof(joystickData)); // Sending entire struct
-
-  delay(20);
-}
+  }
+    
+  if(x <thresholdx && (-thresholdx) < x && y <thresholdx && (-thresholdx) < y){
+    motorLeft.setSpeed(0);
+    motorRight.setSpeed(0);
+    Serial.println("C3");
+    }
+  
+    
+  }
